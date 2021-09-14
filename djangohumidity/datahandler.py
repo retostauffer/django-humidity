24229017f2804c822f32abd44a4b04346f04559d
Reto Stauffer (uberspace) <reto@retostauffer.org>
2021-09-14 12:57:16 +0200

    Trying to select most recent x days only

--- djangohumidity/datahandler.py
+++ djangohumidity/datahandler.py
@@ -1,19 +1,29 @@
 
 
 from django.conf import settings
-from datetime import datetime as dt
+import datetime
 
 import json
 from .models import *
 
-def get_data(sensor_id, param_id):
+def get_data(sensor_id, param_id, ndays = 2):
+
+    if not isinstance(ndays, int):
+        raise TypeError("ndays must be integer")
+    if ndays < 1:
+        raise TypeError("ndays must be larger than 0")
+
+    # Starting timestamp
+    now   = datetime.datetime.now()
+    start = now - datetime.deltatime(ndays)
 
     # Loading the parameter object
     param = Sensor.objects.get(id = sensor_id).parameter_set.all().get(id = param_id)
 
     values    = []
     datetime  = []
-    for rec in param.data_set.all():
+    #for rec in param.data_set.all():
+    for rec in param.data_set.filter(data_datetime__lt = start):
         values.append(rec.data_value)
         datetime.append(rec.data_datetime.isoformat())
 
